fd2f1746bdfdf565c7f0853b42488c1aa2cc251c
Abdullah Alhusaini <abdullah@daarb.dev>
2023-04-03 03:48:49 +0300

    frontend actually posts to server to create users now

--- frontend/app/signup/page.js
+++ frontend/app/signup/page.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 
 export default function Signup() {
   const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [familyName, setFamilyName] = useState("");
 
   return (
     <form
@@ -12,7 +14,12 @@ export default function Signup() {
 
         let res = await fetch("/api/signup/email", {
           method: "post",
-          body: JSON.stringify({ email: email }),
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            email: email,
+            first_name: firstName,
+            family_name: familyName,
+          }),
           credentials: "same-origin",
         });
         res = await res.text();
@@ -20,6 +27,21 @@ export default function Signup() {
         console.log(res);
       }}
     >
+      <label htmlFor="first_name">First name</label>
+      <input
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        id="first_name"
+        name="first name"
+        type="text"
+      />
+      <label htmlFor="family_name">Family Name</label>
+      <input
+        value={familyName}
+        onChange={(e) => setFamilyName(e.target.value)}
+        id="family_name"
+        type="text"
+      />
       <label htmlFor="email">Enter email address</label>
       <input
         value={email}
